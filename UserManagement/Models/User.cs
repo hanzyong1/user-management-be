48cbd8c2edd9c08f5cbeddb11f483b01d9f90699
hanzhiyong <hanzyong1@gmail.com>
2026-03-19 15:33:36 +0800

    add User model

---
+++ UserManagement/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagement.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string FirstName {  get; set; }
+
+        [Required]
+        public string LastName {  get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        public string? ProfilePicPath { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
