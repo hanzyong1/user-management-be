9806357dc85b5b72cb48ac8f8f2c9a1aeab0a9fe
hanzhiyong <hanzyong1@gmail.com>
2026-03-19 19:43:57 +0800

    add UserController with GetUserByIdAsync

---
+++ UserManagement/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserManagement.Services;
+
+namespace UserManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetUserByIdAsync(int id)
+        {
+            var user = await _userService.GetUserByIdAsync(id);
+            
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+    }
+}
