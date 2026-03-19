a1522d9f7a6fd070805bac505167b5ebbfe07e4f
hanzhiyong <hanzyong1@gmail.com>
2026-03-19 15:34:31 +0800

    add DbContext for entity framework

---
+++ UserManagement/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagement.Models;
+
+namespace UserManagement.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
