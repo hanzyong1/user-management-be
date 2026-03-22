417026ccd180a94685d6e99df04d7cae0d1fcd32
hanzhiyong <hanzyong1@gmail.com>
2026-03-22 13:06:59 +0800

    add AuthResponseDto to handle tokens

---
+++ UserManagement/Dtos/AuthDto/AuthResponseDto.cs
@@ -0,0 +1,8 @@
+namespace UserManagement.Dtos.AuthDto
+{
+    public class AuthResponseDto
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
